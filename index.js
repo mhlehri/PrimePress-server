2c259491f5421bf0fdde5c74d3a108b6f6416d31
mhlehri <lehrimirza101@gmail.com>
2023-11-25 17:55:46 +0600

    connected with mongoose and get all articles

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -19,6 +20,19 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
+const schema = new mongoose.Schema({
+  title: String, // String is shorthand for {type: String}
+  tags: String,
+  category: String,
+  article: String,
+  publish_date: { type: Date, default: Date.now },
+  publisher: String,
+  view_count: Number,
+  status: { type: String, default: "pending" },
+});
+
+const Model = mongoose.model("Articles", schema);
+const doc = new Model();
 // custom middleware for verifying token validity
 
 const verifyToken = (req, res, next) => {
@@ -35,30 +49,54 @@ const verifyToken = (req, res, next) => {
   });
 };
 
+mongoose
+  .connect(
+    "mongodb+srv://mahmudhassanlehri:mhlehri101@cluster0.yynznjj.mongodb.net/" +
+      "NewspaperDB?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("db connection established");
+  });
 // Curd operation
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    //? get all articles
 
-app.post("/jwt", (req, res) => {
-  const user = req.body;
-  const token = jwt.sign(user, "secret", { expiresIn: "24h" });
+    app.get("/articles", async (req, res) => {
+      const article = await Model.find({}).exec();
+      console.log(article);
+      res.send(article);
+    });
 
-  res
-    .cookie("token", token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
-    })
-    .send({ success: true });
-});
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, "secret", { expiresIn: "24h" });
 
-app.post("/logout", (req, res) => {
-  res
-    .clearCookie("token", {
-      maxAge: 0,
-      secure: process.env.NODE_ENV === "production" ? true : false,
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-    })
-    .send({ success: true });
-});
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        })
+        .send({ success: true });
+    });
+
+    app.post("/logout", (req, res) => {
+      res
+        .clearCookie("token", {
+          maxAge: 0,
+          secure: process.env.NODE_ENV === "production" ? true : false,
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
+    console.log("");
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("assignment 12 running...");
